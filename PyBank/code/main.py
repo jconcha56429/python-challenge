bdd4778651751b2519c6e655b87ef52f87a4902b
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-04-02 18:33:33 -0700

    profitlosses declared as integer and skip header

--- PyBank/code/main.py
+++ PyBank/code/main.py
@@ -8,18 +8,23 @@ csvpath = os.path.join("..", "Resources", "budget_data.csv")
 # list for data 
 Date=[]
 profitloss=[]
+
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    next(csvreader)
     for row in csvreader:
         # add dates to list
         Date.append(row[0]) 
-        
-        
-        # add profit loss to list 
-        profitloss.append(row[1])
-
-        
-        
+        # add profits to list 
+        profitloss.append(int(row[1]))
+       
 #finishing touches
-datelength = len(Date)
+# Present total number of months
+NewDate = Date[1:]
+datelength = len(NewDate)
 print(f"Total Months: {datelength}")
+# Present sum of profitloss
+total = sum(profitloss)
+print(total)
+
+
