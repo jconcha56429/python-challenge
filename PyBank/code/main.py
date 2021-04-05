1294d935fc1656b7bf1db8571456a5cc8fb6cb65
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-04-05 15:39:23 -0700

    figured out how to create txt file and add to it

--- PyBank/code/main.py
+++ PyBank/code/main.py
@@ -9,7 +9,7 @@ csvpath = os.path.join("..", "Resources", "budget_data.csv")
 # list for data 
 Date=[]
 profitloss=[]
-
+results=[]
 with open(csvpath, newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     next(csvreader)
@@ -20,18 +20,20 @@ with open(csvpath, newline="") as csvfile:
         profitloss.append(int(row[1]))
        
 #finishing touches
+
+
 # Present total number of months
 datelength = len(Date)
-print(f"Total Months: {datelength}")
+str(print(f"Total Months: {datelength}"))
 # Present sum of profitloss
 total = sum(profitloss)
-print(f"Total profit/loss: ${total}")
+str(print(f"Total profit/loss: ${total}"))
 # determine values from one row of profit loss to the next
 change = [j-i for i, j in zip(profitloss[:-1], profitloss[1:])] # all except the first one minus all except the last one 
 # Present average change from one period to another
 averagechange = sum(change) / len(change)
 rounded = round(averagechange,2)
-print(f"Average profit/loss change: ${rounded}")
+str(print(f"Average profit/loss change: ${rounded}"))
 minimum = min(change) # find minimum loss in change 
 maximum = max(change) # find maximum profit in change 
 
@@ -40,7 +42,12 @@ final2 = len(Date)  # there are 86 dates total
 index = change.index(-2196167) # find index 43 + 1 (because there are only 85 changes) in changes to be the minimum change in a year 
 index2 = change.index(1926159) #find index 24 + 1 in changes to be the maximum change in a year 
 
-print(f"Greatest increase in profits: {Date[25]} ${maximum}")
-print(f"Greatest decrease in profits: {Date[44]} ${minimum}")
+str(print(f"Greatest increase in profits: {Date[25]} ${maximum}"))
+str(print(f"Greatest decrease in profits: {Date[44]} ${minimum}"))
 
+results = ((str(f"Total Months: {datelength}")),(f"Total profit/loss: ${total}"))
 
+results_output=os.path.join("PyBank_results.txt")
+with open(results_output,"w") as txt_file:
+    for result in results:
+        txt_file.write(str(result))
