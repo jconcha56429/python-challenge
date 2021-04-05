8af637f0b1ffdf95b29aaa6f148dc0d6e4fa8ba3
jconcha56429 <78937358+jconcha56429@users.noreply.github.com>
2021-04-05 14:03:55 -0700

    finish touches before exporting data to a new file

--- PyBank/code/main.py
+++ PyBank/code/main.py
@@ -32,21 +32,15 @@ change = [j-i for i, j in zip(profitloss[:-1], profitloss[1:])] # all except the
 averagechange = sum(change) / len(change)
 rounded = round(averagechange,2)
 print(f"Average profit/loss change: ${rounded}")
-minimum = min(change)
-maximum = max(change)
-print(minimum)
-print(maximum)
+minimum = min(change) # find minimum loss in change 
+maximum = max(change) # find maximum profit in change 
 
 final = len(change) # there are 85 total changes from one date to another 
-print(final)
 final2 = len(Date)  # there are 86 dates total 
-print(final2)
-# print(maximum) 
-# index +/-1 to get the value 
-index = change.index(-2196167) # find row 43 + 1 (because there are only 85 changes) in changes to be the minimum change in a year 
-print(index)
-index2 = change.index(1926159) #find row 24 + 1 in changes to be the maximum change in a year 
-print(index2)
-for x in range(len(Date)): # locate corresponding dates to changes 
-    print(x,end = " ")
-    print(Date[x])
+index = change.index(-2196167) # find index 43 + 1 (because there are only 85 changes) in changes to be the minimum change in a year 
+index2 = change.index(1926159) #find index 24 + 1 in changes to be the maximum change in a year 
+
+print(f"Greatest increase in profits: {Date[25]} ${maximum}")
+print(f"Greatest decrease in profits: {Date[44]} ${minimum}")
+
+
